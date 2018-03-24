b908e5eaf6c64eb26b5a98ba84390827bde379ed
Vishnu <vichuzzzzs@gmail.com>
2018-03-24 11:48:35 +0530

    client first modification

--- EchoClient.java
+++ EchoClient.java
@@ -5,5 +5,15 @@ public class EchoClient
 {
 public static void main(String args[]) throws Exception
 {
+
+try {
+int Port;
+BufferedReader Buf =new BufferedReader(new
+InputStreamReader(System.in));
+System.out.print(" Enter the Port Address : " );
+Port=Integer.parseInt(Buf.readLine());
+Socket sok=new Socket("localhost",Port);
+
+
 }
 }
