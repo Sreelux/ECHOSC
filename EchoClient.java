eb559f1224c0e284f38f2e5f410876fb72cca9de
Vishnu Ramakishnan <36528906+VishnuRamaT@users.noreply.github.com>
2018-03-27 09:09:20 +0000

    Update EchoClient.java

--- EchoClient.java
+++ EchoClient.java
@@ -25,13 +25,16 @@ InputStreamReader(System.in));
 BufferedReader buf2=new BufferedReader(new
 InputStreamReader(in));
 String str1,str2;
+            while(true){
 System.out.print(" Enter the Message : ");
+    
 str1=buf1.readLine();
 pr.println(str1);
 pr.flush();
 System.out.println(" Message Send Successfully. ");
 str2=buf2.readLine();
 System.out.println(" Message From Server : " + str2);
+            }
      }
   catch(Exception e)
   {
